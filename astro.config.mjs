a6f23af1be65603aeda96dd1fa2fccbbfbfa0123
Egor Miliukov <milukove@gmail.com>
2026-02-14 15:40:57 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://milukove.github.io",
   vite: {
-      plugins: [tailwindcss()],
-	},
-
+    plugins: [tailwindcss()],
+  },
   integrations: [react()],
 });
