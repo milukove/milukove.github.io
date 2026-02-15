1a630503e73db2973e2f8cd7fccad6172193525a
Egor Miliukov <milukove@gmail.com>
2026-02-15 02:24:46 +0200

    Add sitemap integration and update main layout for sitemap link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://miliukov.dev",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
